66bf1dc5cd846a0bf56753d3edf8e1f90cb807ce
Michael Lauenstein <sideburns3000@gmail.com>
2019-09-10 20:17:21 +0200

    added TaskProvider in order to enable compilation of Dialog files

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,66 @@
+/**
+ * Copyright (c) 2019 Michael Lauenstein
+ * License: MIT (see LICENSE file in the root folder)
+ * 
+ * Based on code samples from Microsoft which were also released under the MIT License:
+ * https://github.com/microsoft/vscode-extension-samples/tree/master/task-provider-sample
+ */
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+interface DialogTaskDefinition extends vscode.TaskDefinition {
+	type: "dialog";
+	target: string;
+}
+
+class DialogTaskProvider implements vscode.TaskProvider {
+	private tasks: vscode.Task[] | undefined;
+
+	public async provideTasks(): Promise<vscode.Task[]> {
+		return this.getTasks();
+	}
+
+	public resolveTask(_task: vscode.Task): vscode.Task | undefined {
+		const target: string = _task.definition.target;
+		if (target) {
+			const definition: DialogTaskDefinition = <any>_task.definition;
+			return this.getTask(definition.target, definition);
+		}
+		return undefined;
+	}
+
+	private getTasks(): vscode.Task[] {
+		if (this.tasks !== undefined) {
+			return this.tasks;
+		}
+		const targets: string[] = ["z5", "z8", "zblorb", "aa"];
+		this.tasks = [];
+		targets.forEach(target => {
+			this.tasks!.push(this.getTask(target));
+		});
+		return this.tasks;
+	}
+
+	private getTask(target: string, definition?: DialogTaskDefinition): vscode.Task {
+		if (definition === undefined) {
+			definition = {
+				type: "dialog",
+				target: target
+			};
+		}
+		let taskName: string = "Compile to " + ((target === "aa") ? "aastory" : target);
+		return new vscode.Task(definition, vscode.TaskScope.Workspace, taskName, "dialog", new vscode.ProcessExecution("dialogc", ["-t", target, "${file}", "stdlib.dg"]));
+	}
+}
+
+let dgTaskProvider: vscode.Disposable | undefined;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	dgTaskProvider = vscode.tasks.registerTaskProvider("dialog", new DialogTaskProvider());
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -24,4 +80,8 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (dgTaskProvider) {
+		dgTaskProvider.dispose();
+	}
+}
