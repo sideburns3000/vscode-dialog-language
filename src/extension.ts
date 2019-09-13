d16abbdcf4d7d72de642e3d383c84f46814d44c9
Michael Lauenstein <sideburns3000@gmail.com>
2019-09-13 02:51:37 +0200

    added feature: user can now compile from the file explorer context menu

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,6 @@
  */
 
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 interface DialogTaskDefinition extends vscode.TaskDefinition {
@@ -64,12 +63,13 @@ class DialogTaskProvider implements vscode.TaskProvider {
 
 let dgTaskProvider: vscode.Disposable | undefined;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// this method is called when the extension is activated
+// (compare activationEvents in package.json)
 export function activate(context: vscode.ExtensionContext) {
 
 	dgTaskProvider = vscode.tasks.registerTaskProvider("dialog", new DialogTaskProvider());
 
+	// look out for changes to the compilation configuration settings
 	vscode.workspace.onDidChangeConfiguration(e => {
         if (e.affectsConfiguration('dialog.includeWhenCompiling')) {
 			// we need to re-register the tasks
@@ -78,27 +78,64 @@ export function activate(context: vscode.ExtensionContext) {
 				dgTaskProvider.dispose();
 			}
 			dgTaskProvider = vscode.tasks.registerTaskProvider("dialog", new DialogTaskProvider());
+			context.subscriptions.push(dgTaskProvider);
         }
     });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dialog-language-support" is now active!');
+	context.subscriptions.push(dgTaskProvider);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let cmdCompileToAastory = vscode.commands.registerCommand('dialog.compileToAastory', async (uri: vscode.Uri) => {
+		// This will be executed every time the command is executed
+		compile(uri, "aa");
 	});
 
-	context.subscriptions.push(disposable);
+	let cmdCompileToZ5 = vscode.commands.registerCommand('dialog.compileToZ5', async (uri: vscode.Uri) => {
+		compile(uri, "z5");
+	});
+
+	let cmdCompileToZ8 = vscode.commands.registerCommand('dialog.compileToZ8', async (uri: vscode.Uri) => {
+		compile(uri, "z8");
+	});
+
+	let cmdCompileToZblorb = vscode.commands.registerCommand('dialog.compileToZblorb', async (uri: vscode.Uri) => {
+		compile(uri, "zblorb");
+	});
+
+	context.subscriptions.push(cmdCompileToAastory);
+	context.subscriptions.push(cmdCompileToZ5);
+	context.subscriptions.push(cmdCompileToZ8);
+	context.subscriptions.push(cmdCompileToZblorb);
 }
 
-// this method is called when your extension is deactivated
+/**
+ * Compile a Dialog file to a specified target
+ * @param {vscode.Uri} uri  the file to be compiled
+ * @param {string} intendedTarget the compilation target (aa, z5, z8, zblorb)
+ */
+async function compile(uri: vscode.Uri, intendedTarget: string) {
+	// we open the file in an editor tab, if it's not already open;
+	// this way we ensure that VS Code's ${file} variable is set
+	// correctly and that our tasks, which reference ${file},
+	// will run correctly
+	vscode.commands.executeCommand('vscode.open', uri);
+
+	// we fetch our pre-built tasks (see DialogTaskProvider above)
+	// and execute the task which has the correct compilation target
+	vscode.tasks.executeTask(await vscode.tasks.fetchTasks(new DialogTaskFilter()).then(function (tasks) {
+		return tasks.find(function (task) {
+			return task.definition.target === intendedTarget;
+		})!;
+	}));
+}
+
+class DialogTaskFilter implements vscode.TaskFilter {
+	public type: string = "dialog";
+}
+
+// this method is called when the extension is deactivated
 export function deactivate() {
 	if (dgTaskProvider) {
 		dgTaskProvider.dispose();
